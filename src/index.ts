6eb230181fd5d40778d183a2af5e604b72b24a11
Takumi Maruyama <satouredstone@gmail.com>
2024-01-26 22:42:46 +0900

    Add todo functionality and cache data

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	TODOS: KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -25,8 +25,62 @@ export interface Env {
 	// MY_QUEUE: Queue;
 }
 
+type Todo = {
+	id: number;
+	name: string;
+	completed: boolean;
+}
+
+const defaultData = {
+	todos: [
+		{
+			id: 1,
+			name: "Finish the Cloudflare Workers blog post",
+			completed: false
+		}
+	]
+}
+
+const render = (data: string) => `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>Todos</title>
+  </head>
+  <body>
+    <h1>Todos</h1>
+		<div id="todos"></div>
+		<script>
+      window.todos = ${data}
+      var todoContainer = document.querySelector("#todos")
+      window.todos.forEach(todo => {
+        var el = document.createElement("div")
+        el.textContent = todo.name
+        todoContainer.appendChild(el)
+      })
+    </script>
+  </body>
+</html>
+`;
+
+const setCache = (todo: { todos: Todo[] }, kv: KVNamespace) => kv.put(`data`, JSON.stringify(todo))
+const getCache = (kv: KVNamespace) => kv.get(`data`)
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		let data;
+		const cache = await getCache(env.TODOS)
+		if (!cache) {
+			await setCache(defaultData, env.TODOS)
+			data = defaultData
+		} else {
+			data = JSON.parse(cache)
+		}
+		console.log(data)
+		const response = new Response(render(JSON.stringify(data.todos).replace(/</g, '\\u003c')), {
+			headers: { 'Content-Type': 'text/html' },
+		});
+		return response
 	},
 };
